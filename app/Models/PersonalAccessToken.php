80cbe278ddeab6f5a4953216d271991e4a2464f4
artur.trotskyi <artur.trotskyi@ukr.net>
2025-02-18 15:05:32 +0200

    token creds added

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -14,4 +14,21 @@ class PersonalAccessToken extends SanctumToken
     protected $table = 'personal_access_tokens';
 
     protected $keyType = 'string';
+
+    protected $fillable = [
+        'name',
+        'token',
+        'abilities',
+        'expires_at',
+        'ip',
+        'user_agent',
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        $attributes['ip'] = request()->ip();
+        $attributes['user_agent'] = request()->userAgent();
+
+        parent::__construct($attributes);
+    }
 }
